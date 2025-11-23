8a876849b6eba4828a4ee5f0d07969573e66e0ab
c00312164 <c00312164@setu.ie>
2025-11-23 20:59:35 +0000

    code written needs to be debugged

--- Stage 4/mutex_logging_test.c
+++ Stage 4/mutex_logging_test.c
@@ -1,14 +1,63 @@
-/**
- * This file serves as a test for the mutex logging functionality.
- */
 #include <stdio.h>
-#include "mutex_logging.h"
+#include <stdbool.h>
+
+//cross-platform sleep
+#ifdef _WIN32
+#include <windows.h>
+#define sleep_ms(ms) Sleep(ms)
+#else
+#include <unistd.h>
+#define sleep_ms(ms) usleep(ms * 1000)
+#endif
+
+
+// Check if log.lock exists
+bool lock_exists(void) {
+    // try to open the file
+    FILE *f = fopen("log.lock", "r");
+    // if file doesnt exist
+    if (f == NULL) 
+        return false;
+    fclose(f);
+    return true;
+}
+
+
+int main() {
+    printf("Calling mutex_log...\n");
 
 
 
-int main()
-{
-    // write your test code here
-    printf("Mutex logging test executed.\n");
+    // Wait while lock exists
+    while (lock_exists()) { //  
+        sleep_ms(100);  // 100ms
+    }
+
+    // Create lock file 
+    FILE *lock_file = fopen("log.lock", "w");
+    if (lock_file == NULL) {
+        return;
+    }
+    fclose(lock_file);
+
+    // Open log file
+    FILE *log_file = fopen("system_log.txt", "w"); 
+    if (log_file == NULL) {
+        remove("log.lock");
+        return;
+    }
+
+
+    fprintf(log_file, "%s", "LOG Message");
+
+    fclose(log_file);
+
+    remove("log.lock");
+
+
+    printf("Done. Check system_log.txt.\n");
+
+
+    getchar(); // stops the command window from closing after its done
     return 0;
 }
